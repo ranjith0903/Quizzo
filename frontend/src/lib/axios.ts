2a2132863ef5eedfcc8da231fd87050c17c007ea
Ranjith H K <141330799+ranjith0903@users.noreply.github.com>
2025-02-16 21:42:00 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://quizzo-oyf3.onrender.com/api" : "/api",
-  withCredentials: true, // send cookies to the server
+  baseURL:"https://quizzo-oyf3.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;

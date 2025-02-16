c8d75aef70894a68197149145ca700b0993f75f4
Ranjith H K <141330799+ranjith0903@users.noreply.github.com>
2025-02-16 23:27:34 +0530

    Update quizes.routes.ts

--- backend/src/routes/quizes.routes.ts
+++ backend/src/routes/quizes.routes.ts
@@ -11,19 +11,19 @@ import {
 const router = express.Router();
 
 // Get all quizes
-router.get("/", authMiddleware as RequestHandler, (req: Request, res: Response) => getAllQuizzes(req, res));
+router.get("/", (req: Request, res: Response) => getAllQuizzes(req, res));
 
 // Get quiz by ID
-router.get("/:id", authMiddleware as RequestHandler, (req: Request, res: Response) => getQuizById(req, res));
+router.get("/:id",  (req: Request, res: Response) => getQuizById(req, res));
 
 // Create quiz
-router.post("/", authMiddleware as RequestHandler, (req: Request, res: Response) => createQuiz(req, res));
+router.post("/",  (req: Request, res: Response) => createQuiz(req, res));
 
 // Update quiz
-router.put("/:id", authMiddleware as RequestHandler, (req: Request, res: Response) => updateQuiz(req, res));
+router.put("/:id",  (req: Request, res: Response) => updateQuiz(req, res));
 
 // Delete quiz
-router.delete("/:id", authMiddleware as RequestHandler, (req: Request, res: Response) => deleteQuiz(req, res));
+router.delete("/:id",  (req: Request, res: Response) => deleteQuiz(req, res));
 
 export default router;
 

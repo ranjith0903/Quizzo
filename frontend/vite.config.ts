6e022da64e8c4d88d39fefa5c4cddaee78d5bf24
Ranjith H K <141330799+ranjith0903@users.noreply.github.com>
2025-02-16 23:16:08 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://quizzo-oyf3.onrender.com",
       },
     },
   },

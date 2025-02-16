88100ad33f082eea27682ea402981f6a759be473
Ranjith H K <141330799+ranjith0903@users.noreply.github.com>
2025-02-16 19:46:42 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // CORS setup (allow frontend to send cookies)
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://quizo-38eue2ymy-ranjithkumarhassan-gmailcoms-projects.vercel.app" }));
 app.use(express.json());
 
 // Express session setup

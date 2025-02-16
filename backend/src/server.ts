db98ea86b26763eb88e1a5650e1d11ee9750a04e
Ranjith H K <141330799+ranjith0903@users.noreply.github.com>
2025-02-16 23:36:19 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // CORS setup (allow frontend to send cookies)
-app.use(cors({ credentials: true, origin: "https://quizzo-omega.vercel.app" }));
+app.use(cors({ credentials: true, origin: "https://quizzo-72jz.vercel.app" }));
 app.use(express.json());
 
 // Express session setup
